51d768b435d2a945837cfa0b089e80e5a4b536e4
JulianKropp <kropp.julian@gmail.com>
2023-06-16 11:26:37 +0200

    🔥 Remove journal

--- journal/build/app/models/application_record.rb
+++
@@ -1,6 +0,0 @@
-class ApplicationRecord < ActiveRecord::Base
-  self.abstract_class = true
-
-  include Paginator
-  include RandomRecord
-end
--- journal/build/db/migrate/20190622045354_add_share_token_to_posts.rb
+++
@@ -1,7 +0,0 @@
-class AddShareTokenToPosts < ActiveRecord::Migration[5.1]
-  def change
-    add_column :posts, :sharing_token, :string
-
-    add_index :posts, :sharing_token, unique: true
-  end
-end
