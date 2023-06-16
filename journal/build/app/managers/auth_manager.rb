62650c26e51866803b86607f62f467054bbdb16e
JulianKropp <kropp.julian@gmail.com>
2023-06-16 11:26:37 +0200

    🔥 Remove journal

--- journal/build/app/managers/auth_manager.rb
+++
@@ -1,7 +0,0 @@
-class AuthManager
-  def self.authenticate(username, password)
-    user = User.first
-    return false unless user
-    BCrypt::Password.new(user.password) == password && BCrypt::Password.new(user.username) == username
-  end
-end
